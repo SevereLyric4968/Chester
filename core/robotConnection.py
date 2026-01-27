fa25924f81033a46f2e149c1d467bee7e70892c3
Elisabeth Taylor <et2030@hw.ac.uk>
2026-01-27 12:54:11 +0000

    HE WORKS

--- core/robotConnection.py
+++ core/robotConnection.py
@@ -1,11 +1,16 @@
 from pyniryo import *
-#version mismatch which doesn't seem fixable
+# "pip install "pyniryo<1.2"" <- this fixed it
 robotIpAddress = "10.10.10.10"
-workspaceName = "workspace_1"
-robot = NiryoRobot(robotIpAddress)
-robot.calibrate_auto()
+print("a")
 
-robot.move(PoseObject(90, -57, 211, 127, 77, 85))
-robot.close_connection()
+print("b")
+robot = NiryoRobot(robotIpAddress)
+print("c")
+robot.calibrate_auto()
+print("d")
+pose_target_obj = PoseObject(0.14, 0, 0.2, 0, 0.75, 0) # in meters and radians
+robot.move_pose(pose_target_obj)
+print("e")
+#robot.move_to_home_pose
 
 #test
