c06762ef1b750800e5af46c2b548fd04074d660a
SamStables <stablessam2004@gmail.com>
2026-01-27 11:26:33 +0000

    May be a fix for connecting?

--- core/robotConnection.py
+++ core/robotConnection.py
@@ -1,6 +1,8 @@
 from pyniryo import *
 #version mismatch which doesn't seem fixable
-robot = NiryoRobot("10.10.10.10")
+robotIpAddress = "10.10.10.10"
+workspaceName = "workspace_1"
+robot = NiryoRobot(robotIpAddress)
 robot.calibrate_auto()
 
 robot.move(PoseObject(90, -57, 211, 127, 77, 85))
