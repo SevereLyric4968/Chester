318aab451b567f86ba1580784893976c28f3ab5a
Elisabeth Taylor <et2030@hw.ac.uk>
2026-02-02 18:55:06 +0000

    working movement

--- core/robotConnection.py
+++ core/robotConnection.py
@@ -21,32 +21,49 @@ robot2.calibrate_auto()
 
 # robot 1 movement
 print("d")
-pose_target_obj = PoseObject(0.2, 0, 0.2, 0, 0.75, 0) # in meters and radians
-robot.move_pose(pose_target_obj)
+#pose_target_obj = PoseObject(0.2, 0, 0.2, 0, 0.75, 0) # in meters and radians
+#robot.move_pose(pose_target_obj)
 
 
 #robot 2 movement
 print("e")
-pose_target_obj2 = PoseObject(0.3, 0.1, 0.3, 0, 0.75, 0) # in meters and radians
-robot2.move_pose(pose_target_obj2)
+#pose_target_obj2 = PoseObject(0.3, 0.1, 0.3, 0, 0.75, 0) # in meters and radians
+#robot2.move_pose(pose_target_obj2)
 
 #robot 1 electromagnet 
-robot.setup_electromagnet(pin_electromagnet)
-robot.activate_electromagnet(pin_electromagnet)
-robot.deactivate_electromagnet(pin_electromagnet)
+#robot.setup_electromagnet(pin_electromagnet)
+#robot.activate_electromagnet(pin_electromagnet)
+#robot.deactivate_electromagnet(pin_electromagnet)
 
 
 
 #robot 2 electromagnet
-robot2.setup_electromagnet(pin_electromagnet)
-robot2.activate_electromagnet(pin_electromagnet)
-robot2.deactivate_electromagnet(pin_electromagnet)
+#robot2.setup_electromagnet(pin_electromagnet)
+#robot2.activate_electromagnet(pin_electromagnet)
+#robot2.deactivate_electromagnet(pin_electromagnet)
 
 
 
 #home position
 pose_home = PoseObject(0.14, 0, 0.2, 0, 0.75, 0)
-robot.move_pose(pose_home)
-robot2.move_pose(pose_home)
+#robot.move_pose(pose_home)
+#robot2.move_pose(pose_home)
 
-#test
+#pick and place code
+
+robot.calibrate_auto()
+target_piece = PoseObject(0.2, 0, 0.2, 0, 0.75, 0) # in meters and radians
+piece_destination = PoseObject(0.2, 0, 0.2, 0, 0.75, 0)
+
+robot.setup_electromagnet(pin_electromagnet)
+robot.activate_electromagnet(pin_electromagnet)
+
+robot.move_pose(target_piece)
+
+robot.move_pose(pose_home)
+
+robot.move_pose(piece_destination)
+
+robot.deactivate_electromagnet(pin_electromagnet)
+
+robot.move_pose(pose_home)
