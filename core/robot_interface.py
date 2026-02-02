bed5df3bbecdc2b55b7cf5422f2c7915e6b85ed1
Jay Darroch <jd2043@hw.ac.uk>
2026-02-02 14:58:03 +0000

    map init functions working

--- core/robot_interface.py
+++ core/robot_interface.py
@@ -2,7 +2,6 @@ import chess
 
 
 class RobotInterface:
-    moveMap={}
     def translate(self,move,board):
         pass
         fromSq=move[:2]
@@ -57,3 +56,48 @@ class RobotInterface:
         pass
     def move(self,fromSquare,toSquare):
         pass
+
+    def init_board_map(self,startPos,offset):
+        boardMap={}
+        for rank in range(8):
+            for file in range(8):
+                square=chr(ord("a")+file)+str(rank+1)
+                boardMap[square]=(startPos[0]+offset*file,startPos[1]+offset*rank)
+
+        return boardMap
+
+    def init_storage_map(self,whiteStartPos,blackStartPos,offset):
+        storageMap={}
+        rows=8
+        cols=3
+        layout={
+            'p' : [(0,i) for i in range(8)],
+            'r' : [(1,0),(1,1),(2,0),(2,1)],
+            'n' : [(1,2),(1,3),(2,2),(2,3)],
+            'b' : [(1,4),(1,5),(2,4),(2,5)],
+            'q' : [(1,7),(2,6),(2,7)],
+            'k' : [(1,6)]
+        }
+        for piece in layout:
+            # iterate through piece positions
+            # apply offset between grid squares
+            # append to list in dictionary
+
+            #white
+            storageMap[piece.upper()]=[]
+            for pos in layout[piece]:
+                x=pos[0]*offset+whiteStartPos[0]
+                y=pos[1]*offset+whiteStartPos[1]
+                storageMap[piece.upper()].append((x,y))
+
+            #black
+            #potentially need to flip black but thats a later problem
+            storageMap[piece]=[]
+            for pos in layout[piece]:
+                x=pos[0]*offset+blackStartPos[0]
+                y=pos[1]*offset+blackStartPos[1]
+                storageMap[piece].append((x,y))
+        return storageMap
+ri=RobotInterface()
+storageMap=ri.init_storage_map((0,0),(10,0),1)
+print(storageMap)
