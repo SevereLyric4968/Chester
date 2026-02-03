d92ba21f4d68bcfd6b3a89020253cf065ad916b1
Elisabeth Taylor <et2030@hw.ac.uk>
2026-02-03 14:36:36 +0000

    coin works

--- core/testmoves.py
+++ core/testmoves.py
@@ -36,27 +36,3 @@ robot2.deactivate_electromagnet(pin_electromagnet)
 robot2.move_pose(pose_home)
 
 
-
-
-
-
-#pin_electromagnet = PinID.DO4
-
-#robot = NiryoRobot(robotIpAddress)
-#robot2 = NiryoRobot(robot2IpAddress)
-
-#robot2.calibrate_auto()
-
-#(0.203, -0.339, 0.027)
-
-
-#pickup2_location = PoseObject(0.201, -0.341, 0.001, 0, 1.5, 0) # in meters and radians 0.038
-#robot2.move_pose(pickup2_location)
-
-#robot.setup_electromagnet(pin_electromagnet)
-#robot.activate_electromagnet(pin_electromagnet)
-#robot.deactivate_electromagnet(pin_electromagnet)
-
-#pose_home = PoseObject(0.14, 0, 0.2, 0, 1.5, 0)
-
-#robot.move_pose(pose_home)
