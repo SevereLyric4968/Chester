4e303452caf832cf93930b63162e2f2fe4af060f
Sam Stables <ss2179@hw.ac.uk>
2026-03-04 14:50:31 +0000

    Huge things

--- testbed/robotSounds.py
+++ testbed/robotSounds.py
@@ -4,5 +4,7 @@ robotIpAddress = "10.10.10.10"
 
 robot = NiryoRobot(robotIpAddress)
 robot.calibrate_auto()
+
 robot.set_volume(50)
-robot.say("Hello",0)
+print(robot.get_sounds())
+robot.play_sound("Pink Pony Club-Chappell Roan ).mp3")
