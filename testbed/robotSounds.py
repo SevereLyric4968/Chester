16a9884c2082b6374c84a30e4b4fe8bb4c79869b
Elisabeth Taylor <et2030@hw.ac.uk>
2026-03-05 15:40:25 +0000

    more pickup code

--- testbed/robotSounds.py
+++ testbed/robotSounds.py
@@ -1,8 +1,8 @@
 from pyniryo import *
 
-robotIpAddress = "10.10.10.10"
+robotIpAddress = "192.168.42.1"
 
 robot = NiryoRobot(robotIpAddress)
 robot.calibrate_auto()
 robot.set_volume(50)
-robot.say("Hello",0)
+#robot.say("Hello",0)
