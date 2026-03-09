716b11d0402969b1c3383eaa3294f89b94c93149
Jay Darroch <jd2043@hw.ac.uk>
2026-03-09 15:18:26 +0000

    better now I believe

--- testbed/test_parse_move.py
+++ testbed/test_parse_move.py
@@ -31,30 +31,28 @@ blackOccupancyMap=[
 
 
 def parse_move(beforeMoveMap, whiteOccupancyMap, blackOccupancyMap):
-    if not(beforeMoveMap):beforeMoveMap = [[0 for _ in range(8)] for _ in range(8)]
+
+    if not(beforeMoveMap): beforeMoveMap = [[0 for _ in range(8)] for _ in range(8)]
     afterMoveMap = [[0 for _ in range(8)] for _ in range(8)]
+
     for i in range(8):
         for j in range(8):
             afterMoveMap[i][j] = whiteOccupancyMap[i][j]+blackOccupancyMap[i][j]
-    storageMap = 0
 
-    changeMap = [[0 for _ in range(8)] for _ in range(8)]
-    signedChangeMap = [[0 for _ in range(8)] for _ in range(8)]
+    posDiffs=[]
+    negDiffs=[]
+    posStorageDiffs=[]
+    negStorageDiffs=[]
 
     for i in range(8):
         for j in range(8):
-            if abs(beforeMoveMap[i][j]) == afterMoveMap[i][j]:
-                signedChangeMap[i][j] = 0
-                changeMap[i][j] = 0
-            elif beforeMoveMap[i][j] == 0 and afterMoveMap[i][j] == 1:
-                signedChangeMap[i][j] = 1
-                changeMap[i][j] = 1
+            if beforeMoveMap[i][j] == 0 and afterMoveMap[i][j] == 1:
+                posDiffs.append((i,j))
             elif beforeMoveMap[i][j] != 0 and afterMoveMap[i][j] == 0:
-                signedChangeMap[i][j] = -1
-                changeMap[i][j] = 1
+                negDiffs.append((i,j))
 
-    changeCount = sum(sum(row) for row in changeMap)
-    storageChangeCount = 1
+    changeCount = len(posDiffs)+len(negDiffs)
+    storageChangeCount = len(posStorageDiffs)+len(negStorageDiffs)
 
     isCastling = changeCount == 4
     isPromotion = changeCount == 2 and storageChangeCount >= 2
@@ -66,16 +64,14 @@ def parse_move(beforeMoveMap, whiteOccupancyMap, blackOccupancyMap):
     piece = None
 
     if isCastling:
-        negativeChanges = findGridSquare(signedChangeMap, -1)
         for i in range(2):
-            if negativeChanges[i][1] == 4:
-                fromSq = negativeChanges[i]
-        positiveChanges = findGridSquare(signedChangeMap, 1)
-        toSq = (fromSq[0],1 if positiveChanges[0][1]==1 else 6)
+            if negDiffs[i][1] == 4:
+                fromSq = negDiffs[i]
+        toSq = (fromSq[0],1 if posDiffs[0][1]==1 else 6)
 
 
     elif isPromotion:
-        fromSq = findGridSquare(signedChangeMap, -1)
+        fromSq = negDiffs[0]
         if storageChangeCount == 3:
             for i in range(8):
                 for j in range(8):
@@ -84,21 +80,15 @@ def parse_move(beforeMoveMap, whiteOccupancyMap, blackOccupancyMap):
                         toSq = (i, j)
 
         else:
-            toSq = findGridSquare(signedChangeMap, 1)
-        piece = findGridSquare(storageMap, -1)
+            toSq = posDiffs[0]
+        piece = negStorageDiffs[0]
 
     elif isEnPassant:
-        toSq = findGridSquare(signedChangeMap, 1)[0]
-        for i in range(2):
-            for j in range(2):
-                try:
-                    if signedChangeMap[toSq[0] + pow(-1, i)][toSq[1] + pow(-1, j)] == -1:
-                        fromSq = (toSq[0] + pow(-1, i),toSq[1] + pow(-1, j))
-                except:
-                    continue
+        toSq = posDiffs[0]
+        fromSq = negStorageDiffs[0] if negDiffs[0][0]!=toSq[0] else negStorageDiffs[1]
 
     elif isCapture:
-        fromSq = findGridSquare(signedChangeMap, -1)[0]
+        fromSq = negDiffs[0]
 
         for i in range(8):
             for j in range(8):
@@ -106,23 +96,13 @@ def parse_move(beforeMoveMap, whiteOccupancyMap, blackOccupancyMap):
                     toSq = (i, j)
 
     else:
-        fromSq = findGridSquare(signedChangeMap, -1)[0]
-        toSq = findGridSquare(signedChangeMap, 1)[0]
+        fromSq = negDiffs[0]
+        toSq = posDiffs[0]
 
     # convert from index to board ref
     move = (fromSq, toSq)
-    print(move)
     move = (convert_to_uci(move[0]),convert_to_uci(move[1]))
     return move
-
-
-def findGridSquare(grid, value):
-    gridSquares = []
-    for row in range(8):
-        for col in range(8):
-            if grid[row][col] == value:
-                gridSquares.append((row, col))
-    return(gridSquares)
 
 def convert_to_uci(move):
     square = chr(ord("a") + move[1]) + str(8-move[0])
