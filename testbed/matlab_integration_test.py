b6c3562fc46e7fa06c0ca27ce775b91f278ca54a
brave-samurai <skovvboy@gmail.com>
2026-03-10 14:48:00 +0000

    pushing <3

--- testbed/matlab_integration_test.py
+++ testbed/matlab_integration_test.py
@@ -2,7 +2,7 @@ import subprocess
 
 matlab_program_path = r"C:\Program Files\MATLAB\R2025b\bin\matlab.exe"
 
-matlab_script_path = r"D:\Chester-master\Chester\testbed\process_board.m"
+matlab_script_path = r"D:\Chester-master\Chester\testbed\centering.m"
 
 cmd = f'"{matlab_program_path}" -batch "run(\'{matlab_script_path}\')"'
 
