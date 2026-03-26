3ea6286f0be39e4172b1023fa965e1ad327403f1
Sam Stables <ss2179@hw.ac.uk>
2026-03-26 15:09:41 +0000

    robot manipulator should be changed to use our inverse kinematics (untested)

--- utils/inverse_kinematics.py
+++ utils/inverse_kinematics.py
@@ -19,7 +19,6 @@ def calculateIK(robot, x, y, z):
     z -= 0.0174
     #joint limits in radians -2,949 ≤ Joint 1 ≤ 2,949, -1,83 ≤ Joint 2 ≤ 0,61, -1.34 ≤ Joint 3 ≤ 1,57, -2,089 ≤ Joint 4 ≤ 2,089, -1,919 ≤ Joint 5 ≤ 1.922, -2,53 ≤ Joint 6 ≤ 2,53
     motorOneAngle = (np.arctan2(y,x))
-    print(motorOneAngle)
     a = np.sqrt(pow(x,2)+pow(y,2))
     h=np.sqrt(pow(a,2)+pow(z,2))
     d1 = 0.221
@@ -36,15 +35,15 @@ def calculateIK(robot, x, y, z):
     theta3 = psi + zeta
     motorFiveAngle = -(np.pi - theta3)
     motorSixAngle = 0
-    print(motorOneAngle, motorTwoAngle, motorThreeAngle, motorFourAngle, motorFiveAngle, motorSixAngle)
+    #print(motorOneAngle, motorTwoAngle, motorThreeAngle, motorFourAngle, motorFiveAngle, motorSixAngle)
     robot.move_joints(motorOneAngle, motorTwoAngle, motorThreeAngle, motorFourAngle, motorFiveAngle, motorSixAngle)
 
 if __name__ == "__main__":
     robotIpAddress = "192.168.42.2"
     robot = NiryoRobot(robotIpAddress)
     robot.calibrate_auto()
-    #robot.move_joints(0.3,0,0,0,0,0)
-    calculateIK(robot, 0.2832, 0.0393, 0.140)
+    #robot.move_joints(0,0,0,0,0,0)
+    calculateIK(robot, 0.282, 0.039, 0.210)
 
     #x = 0.3543, y = 0.0524, z = 0.1354 roll = -2.161, pitch = 1.569, yaw = -2.204
     #x = 0.2098, y = 0.0433, z = 0.1357 roll = 3.054, pitch = 1.569, yaw = 2.889
